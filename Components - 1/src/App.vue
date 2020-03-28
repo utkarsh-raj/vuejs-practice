82acf1d4754c678322c7f8a985ba15f9f21cfde4
Utkarsh Raj Singh <u.rajsingh2503@gmail.com>
2020-03-28 18:55:31 +0530

    Components Practicing

---
+++ Components - 1/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="container">
+        <Header/>
+        <hr>
+        <ServerList/>
+        <hr>
+        <Footer/>
+    </div>
+</template>
+
+<script>
+import Header from './components/Header.vue';
+import ServerList from './components/ServerList.vue';
+import Footer from './components/Footer.vue';
+
+export default {
+  name: 'app',
+  components: {
+    Header,
+    ServerList,
+    Footer,
+  },
+};
+</script>
+
+<style>
+
+</style>
